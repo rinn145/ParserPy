76cd4aa7556e73e6163270f33b490699da5c31ba
rinn145 <rt_rio@mail.ru>
2024-03-23 07:39:57 +0600

    addClassRefactore

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 from vulnerability_analyzer import VulnerabilityAnalyzer
 
+
 def main():
     root = tk.Tk()
     analyzer = VulnerabilityAnalyzer(root)
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
